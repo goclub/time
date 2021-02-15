f18a88373db8b73fcb9ec28b2756bf9faa1f629e
Nimo <nimo.jser@gmail.com>
2021-02-15 19:10:54 +0800

    unixMilli

--- readme.md
+++ readme.md
@@ -24,3 +24,10 @@ Format 也有相同的问题。
 故此 goclub/time 提供了一些指定时区的 parse 和 format，目前主要是 China 时区 `time.FixedZone("CST", 8*3600)`
 
 当 json 传递的时间格式不是 RFC3339 而是中国时区年月日时分秒 `2006-01-02 15:04:05`，可以使用 `xtime.ChinaTime` 解析和转换
+
+## UnixMilli
+
+```go
+sometime := time.Date(2020,12,31,23,23,23, 0,LocationChina)
+		assert.Equal(t, UnixMilli(sometime), int64(1609428203000), )
+```
---
+++ unix_test.go
@@ -0,0 +1,18 @@
+package xtime
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestUnixMilli(t *testing.T) {
+	{
+		sometime := time.Date(2020,12,31,23,23,23, 0,LocationChina)
+		assert.Equal(t, UnixMilli(sometime), int64(1609428203000), )
+	}
+	{
+		sometime := time.Date(2020,12,31,23,23,23, 333333333,LocationChina)
+		assert.Equal(t, UnixMilli(sometime), int64(1609428203333), )
+	}
+}
