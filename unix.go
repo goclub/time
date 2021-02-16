651bd24ae7711d7875788b43b33ff6536bfe04d9
Nimo <nimo.jser@gmail.com>
2021-02-16 20:25:46 +0800

    Revert "Update unix.go"
    This reverts commit 6671383b69d337faf2efa66b877ca03f86ce6ffa.

--- unix.go
+++ unix.go
@@ -2,7 +2,8 @@ package xtime
 
 import "time"
 
+// https://stackoverflow.com/questions/24122821/go-golang-time-now-unixnano-convert-to-milliseconds
 func UnixMilli(t time.Time) int64  {
-	return t.UnixNano() / 1e6
+	return t.UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
 }
 
